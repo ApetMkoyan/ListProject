77f68b7b5cefd62e0ccef55b1abf3825005f5cf9
Dose <dose@Doses-MacBook-Pro.local>
2024-05-16 22:54:03 +0400

    Fix ESLint warnings and add missing dependencies in useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import AppRouter from "./Routes/Routes";
 import { selectUsersData } from "./store/slices/UsersData/UsersDataSlice";
-import { useEffect } from "react";
 import { getCurrentUser, getUsersData } from "./store/slices/UsersData/API";
 
 function App() {
-  const dispatch = useDispatch()
-  const {users} = useSelector(selectUsersData)
-  
-  useEffect(()=>{
-    dispatch(getUsersData())
-    dispatch(getCurrentUser())
-  },[])
+  const dispatch = useDispatch();
+  const { users } = useSelector(selectUsersData);
+
+  useEffect(() => {
+    dispatch(getUsersData());
+    dispatch(getCurrentUser());
+  }, [dispatch]);
 
   return (
     <div className="App">
